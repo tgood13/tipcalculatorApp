7ddd6deec10cd7c5df5a431d28b8bd53538ddf17
Troy Good <troygood13@gmail.com>
2020-01-06 10:33:33 -0800

    formatting

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -24,49 +24,39 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         // update stepper values to reflect user defaults
-        
         defaultTipStepper.value = UserDefaults.standard.double(forKey: "defaultTipStepperValue")
-        
         defaultPeopleStepper.value = UserDefaults.standard.double(forKey: "defaultPeopleStepperValue")
         
         // update tip and people labels to reflect user defaults
-        
         defaultTipLabel.text = String(Int(UserDefaults.standard.double(forKey: "defaultTipStepperValue")))
-        
         defaultPeopleLabel.text = String(Int(UserDefaults.standard.double(forKey: "defaultPeopleStepperValue")))
         
         // if label values are zeros, assign them to factory settings and update steppers
-        
         if defaultTipLabel.text == "0" {
             defaultTipLabel.text = "15"
             defaultTipStepper.value = Double(15)
         }
-        
         if defaultPeopleLabel.text == "0" {
             defaultPeopleLabel.text = "1"
             defaultPeopleStepper.value = Double(1)
         }
     }
     
-    // change default tip percentage and update default tip label; sync defaults once complete
     
+    // change default tip percentage and update default tip label; sync defaults once complete
     @IBAction func defaultTipStepper(_ sender: UIStepper) {
         
         UserDefaults.standard.set(sender.value, forKey: "defaultTipStepperValue")
-        
         defaultTipLabel.text = String(Int(sender.value))
-        
         UserDefaults.standard.synchronize()
     }
     
-    // change default # of people and update default tip label; sync defaults once complete
     
+    // change default # of people and update default tip label; sync defaults once complete
     @IBAction func defaultPeopleStepper(_ sender: UIStepper) {
         
         UserDefaults.standard.set(sender.value, forKey: "defaultPeopleStepperValue")
-
         defaultPeopleLabel.text = String(Int(sender.value))
-
         UserDefaults.standard.synchronize()
         
     }
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -29,21 +29,17 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         
         // set cost field to be first responder
-        
         costField.becomeFirstResponder()
         
         // update tip and people stepper values with defaults from Settings
-        
         tipStepper.value = UserDefaults.standard.double(forKey: "defaultTipStepperValue")
         peopleStepper.value = UserDefaults.standard.double(forKey: "defaultPeopleStepperValue")
         
         // update tip and people labels with strings created from defaults in Settings
-        
         tipLabel.text = String(Int(UserDefaults.standard.double(forKey: "defaultTipStepperValue")))
         peopleLabel.text = String(Int(UserDefaults.standard.double(forKey: "defaultPeopleStepperValue")))
         
         // if label values are zeros, assign them to factory settings and update steppers
-        
         if totalTipLabel.text == "0" {
             totalTipLabel.text = "15"
             tipStepper.value = Double(15)
@@ -55,7 +51,6 @@ class ViewController: UIViewController {
         }
         
         // perform necessary math to calculate tip and total values
-        
         let bill = Double(costField.text!) ?? 0
         let tipPercentage = (Double(tipLabel.text!) ?? 0) / 100
         let people = Double(peopleLabel.text!) ?? 1
@@ -63,7 +58,6 @@ class ViewController: UIViewController {
         let total = bill + tip * people
         
         // convert tip and total values to appropriate string format
-        
         let currencyFormatter = NumberFormatter()
         currencyFormatter.usesGroupingSeparator = true
         currencyFormatter.numberStyle = .currency
@@ -73,12 +67,10 @@ class ViewController: UIViewController {
         let totalString = currencyFormatter.string(from: NSNumber(value: total))!
         
         // update total tip and total cost labels
-        
         totalTipLabel.text = tipString
         totalCostLabel.text = totalString
         
         // store bill amount to allow persistence across app restarts
-        
         UserDefaults.standard.set(costField.text, forKey: "billAmount")
         
     }
@@ -86,7 +78,6 @@ class ViewController: UIViewController {
     
     // performs necessary calculations to find total tip and total cost
     // this method is called every time a stepper is updated so that screen reflects accurate values
-    
     @IBAction func calculateTip(_ sender: Any) {
         
         let bill = Double(costField.text!) ?? 0
@@ -100,7 +91,6 @@ class ViewController: UIViewController {
         let total = bill + tip * people
         
         // convert tip and total values to appropriate string format
-        
         let currencyFormatter = NumberFormatter()
         currencyFormatter.usesGroupingSeparator = true
         currencyFormatter.numberStyle = .currency
@@ -110,7 +100,6 @@ class ViewController: UIViewController {
         let totalString = currencyFormatter.string(from: NSNumber(value: total))!
         
         // update corresponding labels
-        
         totalTipLabel.text = tipString
         totalCostLabel.text = totalString
         
@@ -118,22 +107,17 @@ class ViewController: UIViewController {
     }
     
     // change tip percentage, update tip label, and re-calculate tip
-    
     @IBAction func tipStepper(_ sender: UIStepper) {
         
         tipLabel.text = String(Int(sender.value))
-        
         calculateTip(sender)
     }
     
     // change # of people, update people label, and re-calculate tip
-    
     @IBAction func peopleStepper(_ sender: UIStepper) {
         
         peopleLabel.text = String(Int(sender.value))
-        
         calculateTip(sender)
     }
     
 }
-
